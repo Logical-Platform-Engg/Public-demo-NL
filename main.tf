db5fb9d1ee50ae999a06288d452d7d8d8b2af265
saleem404 <cs17saleem.m1997@gmail.com>
2024-04-17 14:19:13 +0530

    testing terrafrom automation

--- main.tf
+++ main.tf
@@ -10,14 +10,6 @@ provider "google" {
   region      = "us-central1"
 }
 
-# Configure Terraform backend to store state file in the existing Google Cloud Storage bucket
-terraform {
-  backend "gcs" {
-    bucket  = "my-project-bucket-test-test"  
-    prefix  = "terraform/state"   
-  }
-}
-
 # Define resources
 resource "google_storage_bucket" "example_bucket" {
   name          = "example-bucket-netlogic"
