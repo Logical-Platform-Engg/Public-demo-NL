8b5a60097715580a6838f03e132e294f50d64420
saleem404 <cs17saleem.m1997@gmail.com>
2024-04-17 14:09:37 +0530

    testing terrafrom automation destory

--- main.tf
+++ main.tf
@@ -1,15 +1,24 @@
-provider "google" {
-  credentials = var.google_credentials
-  project     = var.project
-  region      = "us-central1"
-}
-
 variable "google_credentials" {
   type = string  
 }
 
 variable "project" {}
 
+provider "google" {
+  credentials = var.google_credentials
+  project     = var.project
+  region      = "us-central1"
+}
+
+# Configure Terraform backend to store state file in the existing Google Cloud Storage bucket
+terraform {
+  backend "gcs" {
+    bucket  = "my-project-bucket-test-test"  
+    prefix  = "terraform/state"   
+  }
+}
+
+# Define resources
 resource "google_storage_bucket" "example_bucket" {
   name          = "example-bucket-netlogic"
   location      = "US"
